c72758217e8ffd6a452bb76447a5e8bdfd074437
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2023-06-10 00:28:32 -0300

    Update tanenbaumMP-v2.py

--- tanenbaumMP-v2.py
+++ tanenbaumMP-v2.py
@@ -1,17 +1,21 @@
-mp = [['00000000' for c in range(4)] for b in range(8)] # Para gerar 8 bancos de 4 células cada
+#Definição e criação da MP
+num_celulas = 32
+num_bancos = 8
+celula_por_banco = 4
+mp = [['00000000' for c in range(celula_por_banco)] for b in range(num_bancos)] # Para gerar 8 bancos de 4 células cada
 
-def MPcontroller(address): #Função para 'quebrar' o endereço entre banco e célula
-    if 0 <= address <= 31: #2^5 -> varia de 0 até 31
-        banco = address // 4 #Divisão inteira do endereço pelo nº de células por banco para descobrir qual é o banco desejado
-        celula = address % 4 #Resto da divisão do endereço pelo nº de células por banco para descobrir qual é a célula desejada
+# Função para 'quebrar' o endereço entre banco e célula
+def MPcontroller(address): 
+    if 0 <= address < num_celulas: #2^5 -> varia de 0 até 31
+        banco = address // celula_por_banco #Divisão inteira do endereço pelo nº de células por banco para descobrir qual é o banco desejado
+        celula = address % celula_por_banco #Resto da divisão do endereço pelo nº de células por banco para descobrir qual é a célula desejada
         return banco, celula
-    else:
-        return False
-
-def write(): #Função de escrita
+    
+# Função de escrita
+def write(): 
     while True:
         address = int(input('ENDEREÇO DE 5 bits: ').zfill(5), 2)
-        if 0 <= address <= 31:
+        if 0 <= address < num_celulas:
             banco, celula = MPcontroller(address) #Para as variáveis receberem os valores obtidos pela função MPcontroller
             data = input('INFORMAÇÃO DE 8 bits: ').zfill(8)[:8] #O [:8] serve para deixar o dado com o tamanho de 8bits, retirando, caso ultrapasse, os menos significantes.
             mp[banco][celula] = data
@@ -22,8 +26,8 @@ def write(): #Função de escrita
         else:
             print('ENDEREÇO INVÁLIDO.')
         
-        
-def runSimulation(): #Função para rodar todo o programa
+# Função para rodar todo o programa
+def runSimulation(): 
     while True:
         address = int(input('ENDEREÇO DE 5 bits: ').zfill(5), 2)
         if MPcontroller(address):
